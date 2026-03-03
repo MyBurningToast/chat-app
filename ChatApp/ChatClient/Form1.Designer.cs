ce13bf0f5a416c74c79a9db31551ce8d9bfc2e82
BurningToast <treblaxam@gmail.com>
2026-03-04 10:01:10 +1300

    Use TableLayoutPanels for form layout
    This is to fix broken display DPI scaling

--- ChatApp/ChatClient/Form1.Designer.cs
+++ ChatApp/ChatClient/Form1.Designer.cs
@@ -34,20 +34,26 @@
             txtUsername = new TextBox();
             btnConnect = new Button();
             label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
             SuspendLayout();
             // 
             // txtMessage
             // 
-            txtMessage.Location = new Point(130, 364);
+            txtMessage.Location = new Point(3, 3);
             txtMessage.Name = "txtMessage";
-            txtMessage.Size = new Size(349, 23);
+            txtMessage.Size = new Size(244, 23);
             txtMessage.TabIndex = 0;
             // 
             // btnSend
             // 
-            btnSend.Location = new Point(485, 364);
+            btnSend.Location = new Point(253, 3);
             btnSend.Name = "btnSend";
-            btnSend.Size = new Size(75, 23);
+            btnSend.Size = new Size(87, 23);
             btnSend.TabIndex = 1;
             btnSend.Text = "Send";
             btnSend.UseVisualStyleBackColor = true;
@@ -56,23 +62,23 @@
             // lstMessages
             // 
             lstMessages.FormattingEnabled = true;
-            lstMessages.Location = new Point(130, 9);
+            lstMessages.Location = new Point(192, 3);
             lstMessages.Name = "lstMessages";
-            lstMessages.Size = new Size(430, 349);
+            lstMessages.Size = new Size(379, 334);
             lstMessages.TabIndex = 2;
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(12, 30);
+            txtUsername.Location = new Point(3, 26);
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(112, 23);
+            txtUsername.Size = new Size(152, 23);
             txtUsername.TabIndex = 3;
             // 
             // btnConnect
             // 
-            btnConnect.Location = new Point(12, 59);
+            btnConnect.Location = new Point(3, 59);
             btnConnect.Name = "btnConnect";
-            btnConnect.Size = new Size(75, 23);
+            btnConnect.Size = new Size(75, 22);
             btnConnect.TabIndex = 4;
             btnConnect.Text = "Connect";
             btnConnect.UseVisualStyleBackColor = true;
@@ -81,24 +87,71 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
             label1.Size = new Size(60, 15);
             label1.TabIndex = 5;
             label1.Text = "Username";
             // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(txtUsername, 0, 1);
+            tableLayoutPanel1.Controls.Add(btnConnect, 0, 2);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Location = new Point(3, 3);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 23F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 33F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(180, 84);
+            tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(btnSend, 1, 0);
+            tableLayoutPanel2.Controls.Add(txtMessage, 0, 0);
+            tableLayoutPanel2.Location = new Point(192, 355);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(369, 33);
+            tableLayoutPanel2.TabIndex = 7;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 2;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 32.92683F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 67.07317F));
+            tableLayoutPanel3.Controls.Add(tableLayoutPanel1, 0, 0);
+            tableLayoutPanel3.Controls.Add(tableLayoutPanel2, 1, 1);
+            tableLayoutPanel3.Controls.Add(lstMessages, 1, 0);
+            tableLayoutPanel3.Location = new Point(3, 3);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 2;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 89.3401F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 10.6598988F));
+            tableLayoutPanel3.Size = new Size(574, 394);
+            tableLayoutPanel3.TabIndex = 8;
+            // 
             // Form1
             // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             ClientSize = new Size(579, 401);
-            Controls.Add(label1);
-            Controls.Add(btnConnect);
-            Controls.Add(txtUsername);
-            Controls.Add(lstMessages);
-            Controls.Add(btnSend);
-            Controls.Add(txtMessage);
+            Controls.Add(tableLayoutPanel3);
             Name = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            tableLayoutPanel3.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -109,5 +162,8 @@
         private TextBox txtUsername;
         private Button btnConnect;
         private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
     }
 }
